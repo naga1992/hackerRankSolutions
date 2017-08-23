5ee96d7b08b63520783512425138b18961cb3207
nagsuresh dondapti <nagasuresh92@hotmail.com>
2017-08-23 09:43:27 +0530

    Java RegEx

---
+++ HackerRankPracticeProject/src/HackerRankJavaProblems/solutionJavaRegex.java
@@ -0,0 +1,23 @@
+package HackerRankJavaProblems;
+
+import java.util.Scanner;
+
+public class solutionJavaRegex {
+	public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex
+{
+       public String pattern="^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+    
+}
+
+
